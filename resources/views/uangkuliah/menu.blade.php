78e7781ee12fded83b6aab716a9e957f88aaa686
Gilbert <gilbert778899@gmail.com>
2026-06-08 16:31:59 +0700

    benerin fitur uang kuliah, jdi bpp sama sks ga sama durasinya, serta saat masuk ke uang kuliah, bisa memilih studentid mahasiswa tersebut

--- resources/views/uangkuliah/menu.blade.php
+++ resources/views/uangkuliah/menu.blade.php
@@ -7,19 +7,38 @@
 
     <h2>Menu Uang Kuliah</h2>
 
-    <a href="/uang-kuliah">
-        <button>Tagihan & Pembayaran</button>
-    </a>
+    <form method="GET" action="/uang-kuliah/menu">
+        <label>Pilih Mahasiswa:</label>
+        <select name="student_id">
+            <option value="">-- Pilih Mahasiswa --</option>
+            @foreach(\App\Models\Student::all() as $s)
+                <option value="{{ $s->student_id }}" {{ request('student_id') == $s->student_id ? 'selected' : '' }}>
+                    {{ $s->student_id }} - {{ $s->name }}
+                </option>
+            @endforeach
+        </select>
+        <button type="submit">Pilih</button>
+    </form>
 
-    <br><br>
+    <br>
 
-    <a href="/uang-kuliah/payment-scheme">
-        <button>Pilihan Skema Pembayaran</button>
-    </a>
+    @if(request('student_id'))
+        <a href="/uang-kuliah?student_id={{ request('student_id') }}">
+            <button>Tagihan & Pembayaran</button>
+        </a>
 
-    <a href="/uang-kuliah/dispensasi">
-        <button>Dispensasi</button>
-    </a>
+        <br><br>
+
+        <a href="/uang-kuliah/payment-scheme?student_id={{ request('student_id') }}">
+            <button>Pilihan Skema Pembayaran</button>
+        </a>
+
+        <br><br>
+
+        <a href="/uang-kuliah/dispensasi?student_id={{ request('student_id') }}">
+            <button>Dispensasi</button>
+        </a>
+    @endif
 
 </body>
 </html>
